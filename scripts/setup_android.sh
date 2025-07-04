b59104dac670568b8e19c4bc9f7b7cb490f4e922
AliHassan <ceo.alihassan.2004@gmail.com>
2025-07-04 23:05:35 +0500

    Initial commit with AAR and source code

--- scripts/setup_android.sh
+++ scripts/setup_android.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Download Android AAR
-ANDROID_URL="https://github.com/sk3llo/ffmpeg_kit_flutter/releases/download/7.0/ffmpeg-kit-full-gpl-7.0.aar"
+ANDROID_URL="https://artifactory.appodeal.com/appodeal-public/com/arthenica/ffmpeg-kit-min-gpl/6.0-2/ffmpeg-kit-min-gpl-6.0-2.aar"
 mkdir -p libs
 curl -L $ANDROID_URL -o libs/com.arthenica.ffmpegkit-flutter-7.0.aar
